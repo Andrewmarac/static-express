abe53219d85986b5997e567c7c37f58ad4e677c5
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2020-11-05 14:11:04 +0000

    finito

--- EXPRESS_STATIC_FILES/index.js
+++ EXPRESS_STATIC_FILES/index.js
@@ -6,6 +6,16 @@ app.get('/',function(req,res){
  res.sendFile(path.join(__dirname, 'index.html')); //__dirname : Ritorna la cartella del progetto
 });
 
+app.get('/about',function(req,res){
+ res.sendFile(path.join(__dirname,'about.html')); //__dirname : Ritorna la cartella del
+
+});
+
+app.get('/sitemap',function(req,res){
+ res.sendFile(path.join(__dirname,'sitemap.html')); //__dirname : Ritorna la cartella del
+
+});
+
 app.listen(3000, function () {
  console.log('Example app listening on port 3000!');
 });
